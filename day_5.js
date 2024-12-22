f08204d848860f74da773af51c6eb0f6bd71a30c
César Cordeiro <cesar88.20@gmail.com>
2024-12-22 00:26:26 -0300

    Day 5 part 2

--- day_5.js
+++ day_5.js
@@ -12,7 +12,7 @@ const rules = lines.slice(0, indexEmptyLine).map((rule) => {
   return rule.split('|').map((pageNumber) => parseInt(pageNumber));
 });
 
-const pages = lines.slice(indexEmptyLine + 1, -1)
+const pages = lines.slice(indexEmptyLine + 1, lines.length)
   .map(
     (sequence) => {
       return sequence.split(',').map(
@@ -41,12 +41,18 @@ function hasDependency(pageNumber, dependencies) {
   return dependencies.indexOf(pageNumber) > -1;
 }
 
-function hasNoLock(subPages, dependencies) {
+function hasLock(subPages, dependencies) {
+  return countLocks(subPages, dependencies) > 0;
+}
+
+function countLocks(subPages, dependencies) {
   if (dependencies === void 0 || dependencies.length == 0) {
-    return true;
+    return false;
   }
 
-  return subPages.every((pageNumber) => !hasDependency(pageNumber, dependencies));
+  return subPages.filter(
+    (pageNumber) => hasDependency(pageNumber, dependencies)
+  ).length;
 }
 
 function partOne() {
@@ -59,14 +65,42 @@ function partOne() {
         const nextPagesInSequence = sequence.slice(idx + 1, sequence.length);
         const dependenciesForCurrentPage = dependencies.get(page);
 
-        return hasNoLock(nextPagesInSequence, dependenciesForCurrentPage);
+        return !hasLock(nextPagesInSequence, dependenciesForCurrentPage);
       });
 
   }).map(
-    (sequence) => sequence[parseInt(sequence.length/2)])
-  .reduce(
+    (sequence) => sequence[parseInt(sequence.length/2)]
+  ).map(
+    (number) => isNaN(number) ? 0 : number
+  ).reduce(
+    (acc, value) => acc + value
+  );
+}
+
+function partTwo() {
+  const dependencies = computeDependencies();
+
+  return pages.filter((sequence) => {
+    return sequence.some(
+      (page, idx) => {
+        const nextPagesInSequence = sequence.slice(idx + 1, sequence.length);
+        const dependenciesForCurrentPage = dependencies.get(page);
+
+        return hasLock(nextPagesInSequence, dependenciesForCurrentPage);
+      });
+  }).map((sequence) => {
+    return sequence.toSorted(
+      (a, b) => {
+        return countLocks(sequence, dependencies.get(a)) -
+               countLocks(sequence, dependencies.get(b));
+      }
+    )
+  }).map(
+    (sequence) => sequence[parseInt(sequence.length/2)]
+  ).reduce(
     (acc, value) => acc + value
   );
 }
 
 console.log(`Part 1: ${partOne()}`);
+console.log(`Part 2: ${partTwo()}`);
