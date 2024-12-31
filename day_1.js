0d4ba53305fca0d6e32325a00368e3d34b6adf64
César Cordeiro <cesar88.20@gmail.com>
2024-12-30 22:17:26 -0300

    Day 1 part 1

---
+++ day_1.js
@@ -0,0 +1,25 @@
+'use strict';
+
+import fs from 'fs';
+
+const data = fs.readFileSync('./resources/day1.in', 'utf-8');
+
+const numbers = data.split("\n").map((line) => {
+  return line.split(/\s+/).map((e) => parseInt(e));
+});
+
+const firstColumn = numbers.map((e) => e[0]);
+const secondColumn = numbers.map((e) => e[1]);
+
+function partOne() {
+  const sortFunc = (a, b) => a - b;
+
+  const firstSorted = [...firstColumn].sort(sortFunc);
+  const secondSorted = [...secondColumn].sort(sortFunc);
+
+  return firstSorted.map((e, idx) => {
+    return Math.abs(e - secondSorted[idx]);
+  }).reduce((acc, e) => acc + e);
+}
+
+console.log(`Part 1: ${partOne()}`);
