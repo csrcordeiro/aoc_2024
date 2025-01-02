8dbc60b8ce0f69787b95537370526a6242a4ff0d
César Cordeiro <cesar88.20@gmail.com>
2025-01-02 11:58:44 -0300

    Day3 part 2

--- day_3.js
+++ day_3.js
@@ -27,6 +27,31 @@ function extractAllPatterns(expression, regex) {
   return matches;
 }
 
+function tokenize(tokens) {
+  let mustCount = true;
+  const selection = [];
+
+  for (let i = 0; i < tokens.length; i++) {
+    const token = tokens[i];
+
+    if (token.includes("don't")) {
+      mustCount = false;
+      continue;
+    }
+
+    if (token.includes("do")) {
+      mustCount = true;
+      continue;
+    }
+
+    if (token.includes("mul") && mustCount) {
+      selection.push(token);
+    }
+  }
+
+  return selection;
+}
+
 function partOne() {
   const extract = /mul\(\d+,\s?\d+\)/gi;
 
@@ -35,4 +60,15 @@ function partOne() {
     .reduce((acc, number) => acc + number);
 }
 
+function partTwo() {
+  const extract = /(do\(\))|(don't\(\))|(mul\(\d+,\s?\d+\))/gi;
+
+  return tokenize(
+    extractAllPatterns(expressions, extract)
+  )
+    .flatMap((find) => multiply(find))
+    .reduce((acc, number) => acc + number);
+}
+
 console.log(`Part 1: ${partOne()}`);
+console.log(`Part 2: ${partTwo()}`);
