4c88d719d17c01ad58c28f7e4e2213bb55aeb1bf
César Cordeiro <cesar88.20@gmail.com>
2025-01-16 20:04:35 -0300

    Day 12 part 2 python

--- python/day_12.py
+++ python/day_12.py
@@ -110,21 +110,61 @@ def calculate_perimeter(point, points):
     return empty_counter
 
 
+# I shamelessly copied this algorithm from reddit.
+# https://www.reddit.com/r/adventofcode/comments/1hcdnk0/comment/m1trm2r/
+#
+# This is an interesting idea of using the string pattern to find corners.
+def get_sides(cluster):
+    sides = 0
+
+    edge_coord_corners = set()
+    for x, y in cluster:
+        for dx, dy in [(.5, .5), (.5, -.5), (-.5, .5), (-.5, -.5)]:
+            edge_coord_corners.add((x + dx, y + dy))
+
+    for x, y in edge_coord_corners:
+        pattern = ""
+        for dx, dy in [(.5, .5), (.5, -.5), (-.5, .5), (-.5, -.5)]:
+            pattern += "X" if (x+dx, y+dy) in cluster else "O"
+        if pattern in ("OXXO", "XOOX"):
+            # When an edge coord is two the region meets itself all catty-corner
+            sides += 2
+        elif pattern.count("X") == 3 or pattern.count("O") == 3:
+            # For when an edge coord is an interior or exterior corner.
+            sides += 1
+
+    return sides
+
+
+def calculate_region_price(cluster, discount=False):
+    area = len(cluster)
+    perimeter = 0
+
+    if discount:
+        perimeter = get_sides(cluster)
+
+    else:
+        for point in cluster:
+            perimeter += calculate_perimeter(point, cluster)
+
+    return area * perimeter
+
+
 def part_one():
-    plants = map_plants()
-    price = 0
+    return sum((
+        calculate_region_price(cluster, discount=False)
+        for _, clusters in map_plants().items()
+        for cluster in clusters
+    ))
 
-    for _, clusters in plants.items():
-        for cluster in clusters:
-            area = len(cluster)
-            perimeter = 0
 
-            for point in cluster:
-                perimeter += calculate_perimeter(point, cluster)
-
-            price += area * perimeter
-
-    return price
+def part_two():
+    return sum((
+        calculate_region_price(cluster, discount=True)
+        for _, clusters in map_plants().items()
+        for cluster in clusters
+    ))
 
 
 print(f"Part 1: {part_one()}")
+print(f"Part 2: {part_two()}")
